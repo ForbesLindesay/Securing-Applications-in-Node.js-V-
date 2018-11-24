6d799e712d3fdebeb757ffbedd4b5a90c1a4e9f8
Forbes Lindesay <forbes@lindesay.co.uk>
2018-11-24 16:20:26 +0000

    Support postgres database

--- demo-project/src/safe-database.js
+++ demo-project/src/safe-database.js
@@ -1,14 +1,26 @@
 import assert from 'assert';
+import connect from '@moped/db-pg';
 import {SQLQuery} from '@moped/sql';
 import loadRaw from './database-connection';
 
 export default function load(tables) {
-  Object.keys(tables).forEach(tableName => {
-    assert(tables[tableName] instanceof SQLQuery);
-  });
-  const db = loadRaw(tables);
-  return async query => {
-    assert(query instanceof SQLQuery);
-    return db(query);
-  };
+  if (process.env.DATABASE_URL) {
+    const db = connect(process.env.DATABASE_URL);
+    Object.keys(tables).forEach(tableName => {
+      db.query(tables[tableName]).catch(() => {
+        // table may already exist, you must run migrations
+        // manually in production
+      });
+    });
+    return db.query.bind(db);
+  } else {
+    Object.keys(tables).forEach(tableName => {
+      assert(tables[tableName] instanceof SQLQuery);
+    });
+    const db = loadRaw(tables);
+    return async query => {
+      assert(query instanceof SQLQuery);
+      return db(query);
+    };
+  }
 }
